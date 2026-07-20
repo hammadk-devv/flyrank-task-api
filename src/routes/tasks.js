1c39fc214bca895104bf54b603117d5e04307596
Hammad Khan <hammadrx8@gmail.com>
2026-07-20 23:33:03 +0500

    Stage 4: Full CRUD

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -8,7 +8,7 @@ router.get("/", (req, res) => {
   res.json(tasks);
 });
 
-// GET single task by ID
+// GET task by ID
 router.get("/:id", (req, res) => {
   const id = parseInt(req.params.id);
 
@@ -42,6 +42,57 @@ router.post("/", (req, res) => {
   tasks.push(newTask);
 
   res.status(201).json(newTask);
+});
+
+// PUT update a task
+router.put("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const task = tasks.find((task) => task.id === id);
+
+  if (!task) {
+    return res.status(404).json({
+      error: `Task ${id} not found`,
+    });
+  }
+
+  const { title, done } = req.body;
+
+  if (
+    (title !== undefined && title.trim() === "") ||
+    (title === undefined && done === undefined)
+  ) {
+    return res.status(400).json({
+      error: "Provide a valid title or done status",
+    });
+  }
+
+  if (title !== undefined) {
+    task.title = title;
+  }
+
+  if (done !== undefined) {
+    task.done = done;
+  }
+
+  res.json(task);
+});
+
+// DELETE task
+router.delete("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const index = tasks.findIndex((task) => task.id === id);
+
+  if (index === -1) {
+    return res.status(404).json({
+      error: `Task ${id} not found`,
+    });
+  }
+
+  tasks.splice(index, 1);
+
+  res.status(204).send();
 });
 
 module.exports = router;
